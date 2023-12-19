730a2506af033eca6c021b76474b0e81022ab4ef
dEfy310 <musinov_2004@mail.ru>
2023-12-20 00:13:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#PZ №1
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
+
+x1 = [0, 35.25]
+y1 = [28.2, 0]
+plt.title('Графическое решение задачи линейного программирования')
+plt.scatter(x1, y1, c='k')
+plt.plot(x1, y1, c='k')
+plt.grid()
+
+
+
+
+
+plt.show()
